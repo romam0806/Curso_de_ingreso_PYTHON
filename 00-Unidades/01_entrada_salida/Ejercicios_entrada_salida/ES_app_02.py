0b6bdea04e456728a8888b2f41d65ba94500d687
roman0806 <nocettiroman@gmail.com>
2024-02-05 22:49:23 -0300

    git commit "ejercicio 2 completado"

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:Román
+apellido:Nocetti
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -27,7 +27,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = prompt(title="pregunta",prompt="cual es su nombre?")
+        alert(title="INFO",mensaje=nombre)
         
         
     
